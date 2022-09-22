ed6a12795570f849af048cf85221554960ec38ce
Nick <ncanino@nrchealth.com>
2022-09-22 15:28:13 -0400

    Update aws_native_pipe_test-stack.ts

--- lib/aws_native_pipe_test-stack.ts
+++ lib/aws_native_pipe_test-stack.ts
@@ -1,10 +1,20 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { CodePipeline, CodePipelineSource, ShellStep, Step } from 'aws-cdk-lib/pipelines';
 
-export class AwsNativePipeTestStack extends cdk.Stack {
+export class CiCdAwsPipelineDemoStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    new CodePipeline(this, 'Pipeline', {
+      pipelineName: 'TestPipeline',
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub('ncaninoNRC/awsNativePipeTest', 'main'), 
+        commands: ['npm ci', 
+                   'npm run build', 
+                   'npx cdk synth']
+      })
+    });
 
     // The code that defines your stack goes here
 
